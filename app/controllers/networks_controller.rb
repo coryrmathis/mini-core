713fb24a8d71d9ea14124693a867fc5cad976213
Cory Mathis <corymathis@MacBook-Pro-3.local>
2022-06-01 13:11:24 -0500

    Define initial controller.

---
+++ app/controllers/networks_controller.rb
@@ -0,0 +1,26 @@
+class NetworksController < ApplicationController
+
+  def index
+    @networks = Network.active
+  end
+
+  def show
+  
+  end
+
+  def new 
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update 
+  end
+
+  def destroy
+  end
+
+end
